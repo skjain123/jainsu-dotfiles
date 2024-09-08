bbc24af0328ce692ed820bca6b0ddda5b7faeb35
Sunil Jain <suniljain0504@gmail.com>
2024-09-07 21:27:15 -0700

    tweaked awesomewm window layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -22,26 +22,7 @@ require("awful.hotkeys_popup.keys")
 debian = require("debian.menu")
 has_fdo, freedesktop = pcall(require, "freedesktop")
 
--- Table of layouts to cover with awful.layout.inc, order matters.
-awful.layout.layouts = {
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
-    awful.layout.suit.tile.bottom,
-    awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
-    awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral,
-    awful.layout.suit.spiral.dwindle,
-    awful.layout.suit.max,
-    awful.layout.suit.max.fullscreen,
-    awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
-    awful.layout.suit.floating,
-    -- awful.layout.suit.corner.ne,
-    -- awful.layout.suit.corner.sw,
-    -- awful.layout.suit.corner.se,
-}
--- }}}
+
 
 -- Themes define colours, icons, font and wallpapers.
 -- beautiful.init(gears.filesystem.get_themes_dir() .. "mytheme.lua")
--- .config/awesome/wibar.lua
+++ .config/awesome/wibar.lua
@@ -70,9 +70,29 @@ awful.screen.connect_for_each_screen(function(s)
         l.max.fullscreen,
     }
 
+    awful.layout.layouts = layouts
+
+    -- Table of layouts to cover with awful.layout.inc, order matters.
+    -- awful.layout.layouts = {
+    --     awful.layout.suit.tile,
+    --     awful.layout.suit.tile.left,
+    --     awful.layout.suit.tile.bottom,
+    --     awful.layout.suit.tile.top,
+    --     awful.layout.suit.fair,
+    --     awful.layout.suit.fair.horizontal,
+    --     awful.layout.suit.spiral,
+    --     awful.layout.suit.spiral.dwindle,
+    --     awful.layout.suit.max,
+    --     awful.layout.suit.max.fullscreen,
+    --     awful.layout.suit.magnifier,
+    --     awful.layout.suit.corner.nw,
+    --     awful.layout.suit.floating,
+    -- }
+    -- }}}
+
     -- Each screen has its own tag table.
     local tagnames = beautiful.tagnames or { "1", "2", "3", "4", "5", "6", "7", "8", "9" }
-    awful.tag(tagnames, s, layouts)
+    awful.tag(tagnames, s, layouts[1])
 
     s.mypromptbox = awful.widget.prompt()
     -- Create an imagebox widget which will contain an icon indicating which layout we're using.
